3b93d2023de16bf36be6f2adf08c4ed20a249194
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-18 21:21:12 -0600

    Completed RPS && MakingChange exercises
    - Completed Rock Paper Scissors exercise
    - Completed Making Change exercise

--- src/making_change/making_change.py
+++ src/making_change/making_change.py
@@ -1,17 +1,30 @@
-#!/usr/bin/python
-
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    ways = [1] + [0] * amount
+    for coin in denominations:
+        for i in range(coin, amount + 1):
+            ways[i] += ways[i - coin]
+    print(ways[amount])
+
+
+def testing():
+    example = [0] + [1] * 10
+    print(example)
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    testing()
+    # making_change(200, [1, 5, 10, 25, 50])
+
+# if __name__ == "__main__":
+#     # Test our your implementation from the command line
+#     # with `python making_change.py [amount]` with different amounts
+#     if len(sys.argv) > 1:
+#         denominations = [1, 5, 10, 25, 50]
+#         amount = int(sys.argv[1])
+#         print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations),
+#                                                                      amount=amount))
+#     else:
+#         print("Usage: making_change.py [amount]")
--- src/rock_paper_scissors/rps.py
+++ src/rock_paper_scissors/rps.py
@@ -3,8 +3,6 @@ from itertools import *
 
 
 def rock_paper_scissors(n, arr=['rock', 'paper', 'scissors']):
-    if len(arr) == 0:
-        return 0
     # Convert the list of tuples into a list of lists (This applies to all returns below)
     # This does not show all possible permutations possible
     # return [list(ele) for ele in list(combinations_with_replacement(arr, n))]
@@ -13,16 +11,9 @@ def rock_paper_scissors(n, arr=['rock', 'paper', 'scissors']):
     #
     # What I was looking for is called Cartesian Product.
     # This provides repeating permutation and all possible outcomes
+
     return [list(ele) for ele in list(product(arr, repeat=n))]
 
 
 if __name__ == "__main__":
-    print(rock_paper_scissors(2))
-
-
-# if __name__ == "__main__":
-#     if len(sys.argv) > 1:
-#         num_plays = int(sys.argv[1])
-#         print(rock_paper_scissors(num_plays))
-#     else:
-#         print('Usage: rps.py [num_plays]')
+    print(rock_paper_scissors(8))
